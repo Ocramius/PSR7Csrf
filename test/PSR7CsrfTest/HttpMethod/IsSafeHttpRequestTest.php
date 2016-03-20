485525735d901bf4fee15ff6ee3e70679f3ab608
Marco Pivetta <ocramius@gmail.com>
2016-03-20 14:45:40 +0100

    Basic spec for a `IsSafeHttpMethod` API

---
+++ test/PSR7CsrfTest/HttpMethod/IsSafeHttpRequestTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PSR7CsrfTest\HttpMethod;
+
+use Lcobucci\JWT\Signer;
+use PHPUnit_Framework_TestCase;
+use Psr\Http\Message\RequestInterface;
+use PSR7Csrf\HttpMethod\IsSafeHttpMethod;
+
+final class IsSafeHttpMethodTest extends PHPUnit_Framework_TestCase
+{
+    public function testSafeMethods(array $safeMethods, string $httpMethod, bool $expectedResult)
+    {
+        $request = $this->getMock(RequestInterface::class);
+
+        $request->expects(self::any())->method('getMethod')->willReturn($httpMethod);
+
+        self::assertSame($expectedResult, (new IsSafeHttpMethod(...$safeMethods))->__invoke($httpMethod));
+    }
+
+    public function httpMethodsProvider() : array
+    {
+        return [
+            'empty' => [
+                [],
+                'GET',
+                false,
+            ],
+            'GET only' => [
+                ['GET'],
+                'GET',
+                true,
+            ],
+        ];
+    }
+}
