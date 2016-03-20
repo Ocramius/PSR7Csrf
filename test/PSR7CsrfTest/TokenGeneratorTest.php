1dd4b93bf7822d4d288160f231078c0249638921
Marco Pivetta <ocramius@gmail.com>
2016-03-20 15:52:45 +0100

    Signed tokens should be generated as CSRF tokens

--- test/PSR7CsrfTest/TokenGeneratorTest.php
+++ test/PSR7CsrfTest/TokenGeneratorTest.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace PSR7CsrfTest;
 
+use Lcobucci\JWT\Parser;
 use Lcobucci\JWT\Signer;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 use PHPUnit_Framework_TestCase;
+use Psr\Http\Message\ServerRequestInterface;
 use PSR7Csrf\Exception\InvalidExpirationTimeException;
 use PSR7Csrf\Session\ExtractUniqueKeyFromSessionInterface;
 use PSR7Csrf\TokenGenerator;
+use PSR7Session\Session\SessionInterface;
 
 /**
  * @covers \PSR7Csrf\TokenGenerator
@@ -29,7 +33,7 @@ final class TokenGeneratorTest extends PHPUnit_Framework_TestCase
 
         $this->expectException(InvalidExpirationTimeException::class);
 
-        new TokenGenerator($signer, $extractUniqueKeyFromSession, $invalidExpirationTime);
+        new TokenGenerator($signer, $extractUniqueKeyFromSession, $invalidExpirationTime, 'session');
     }
 
     public function invalidExpirationTimeProvider() : array
@@ -38,6 +42,43 @@ final class TokenGeneratorTest extends PHPUnit_Framework_TestCase
             [0],
             [-1],
             [-200],
+        ];
+    }
+
+    /**
+     * @dataProvider validExpirationTimeProvider
+     *
+     * @param int $validExpirationTime
+     */
+    public function testWillGenerateAValidJWTToken(int $validExpirationTime)
+    {
+        $signer = new Sha256();
+        /* @var $extractUniqueKeyFromSession ExtractUniqueKeyFromSessionInterface|\PHPUnit_Framework_MockObject_MockObject */
+        $extractUniqueKeyFromSession = $this->getMock(ExtractUniqueKeyFromSessionInterface::class);
+        /* @var $session SessionInterface */
+        $session = $this->getMock(SessionInterface::class);
+        /* @var $request ServerRequestInterface|\PHPUnit_Framework_MockObject_MockObject */
+        $request = $this->getMock(ServerRequestInterface::class);
+        $sessionAttribute = uniqid('session', true);
+
+        $generator = new TokenGenerator($signer, $extractUniqueKeyFromSession, $validExpirationTime, $sessionAttribute);
+        $secretKey = uniqid('secretKey', true);
+
+        $request->expects(self::any())->method('getAttribute')->with($sessionAttribute)->willReturn($session);
+        $extractUniqueKeyFromSession->expects(self::any())->method('__invoke')->with($session)->willReturn($secretKey);
+
+        $tokenString = $generator->__invoke($request);
+
+        $token = (new Parser())->parse($tokenString);
+
+        self::assertTrue($token->verify($signer, $secretKey));
+    }
+
+    public function validExpirationTimeProvider() : array
+    {
+        return [
+            [10],
+            [100],
         ];
     }
 }
