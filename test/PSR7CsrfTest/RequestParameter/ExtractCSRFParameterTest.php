9abbec1519eea02a79dec8ed533c5a5e522a7a31
Marco Pivetta <ocramius@gmail.com>
2016-03-20 16:33:27 +0100

    Basic spec for an `ExtractCSRFParameter` utility

---
+++ test/PSR7CsrfTest/RequestParameter/ExtractCSRFParameterTest.php
@@ -0,0 +1,54 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PSR7CsrfTest\RequestParameter;
+
+use Lcobucci\JWT\Signer;
+use PHPUnit_Framework_TestCase;
+use Psr\Http\Message\ServerRequestInterface;
+use PSR7Csrf\Exception\InvalidRequestParameterNameException;
+use PSR7Csrf\RequestParameter\ExtractCSRFParameter;
+
+/**
+ * @covers \PSR7Csrf\RequestParameter\ExtractCSRFParameter
+ */
+final class ExtractCSRFParameterTest extends PHPUnit_Framework_TestCase
+{
+    public function testRejectsEmptyRequestParameterName()
+    {
+        $this->expectException(InvalidRequestParameterNameException::class);
+
+        new ExtractCSRFParameter('');
+    }
+
+    /**
+     * @dataProvider requestBodyProvider
+     *
+     * @param string            $requestParameter
+     * @param null|object|array $body
+     * @param string            $expectedExtractedValue
+     *
+     * @return void
+     */
+    public function testExtraction(string $requestParameter, $body, string $expectedExtractedValue)
+    {
+        /* @var $request ServerRequestInterface|\PHPUnit_Framework_MockObject_MockObject */
+        $request = $this->getMock(ServerRequestInterface::class);
+
+        $request->expects(self::any())->method('getParsedBody')->willReturn($body);
+
+        self::assertSame($expectedExtractedValue, (new ExtractCSRFParameter($requestParameter))->__invoke($request));
+    }
+
+    public function requestBodyProvider()
+    {
+        return [
+            'null body' => [
+                'request parameter name',
+                null,
+                '',
+            ],
+        ];
+    }
+}
