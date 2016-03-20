a823ec1fdadec5f9e9f494c1fda2b005a91ea2df
Marco Pivetta <ocramius@gmail.com>
2016-03-20 16:41:06 +0100

    Implemented `ExtractCSRFParameter` as per specification

---
+++ src/PSR7Csrf/RequestParameter/ExtractCSRFParameter.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PSR7Csrf\RequestParameter;
+
+use Lcobucci\JWT\Signer;
+use Psr\Http\Message\ServerRequestInterface;
+use PSR7Csrf\Exception\InvalidRequestParameterNameException;
+
+final class ExtractCSRFParameter
+{
+    /**
+     * @var string
+     */
+    private $csrfDataKey;
+
+    public function __construct(string $csrfDataKey)
+    {
+        if ('' === $csrfDataKey) {
+            throw InvalidRequestParameterNameException::fromEmptyRequestParameterName();
+        }
+
+        $this->csrfDataKey = $csrfDataKey;
+    }
+
+    public function __invoke(ServerRequestInterface $request) : string
+    {
+        /* @var $requestBody array */
+        $requestBody = $request->getParsedBody();
+
+        if (is_object($requestBody) && array_key_exists($this->csrfDataKey, (array) $requestBody)) {
+            $arrayBody = (array) $requestBody;
+
+            return $this->ensureThatTheValueIsAString($arrayBody[$this->csrfDataKey]);
+        }
+
+        if (is_array($requestBody) && array_key_exists($this->csrfDataKey, $requestBody)) {
+            return $this->ensureThatTheValueIsAString($requestBody[$this->csrfDataKey]);
+        }
+
+        return '';
+    }
+
+    private function ensureThatTheValueIsAString($value) : string
+    {
+        if (! is_string($value)) {
+            return '';
+        }
+
+        return $value;
+    }
+}
